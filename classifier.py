ff010175da42b98e622d2e2f8d3f4d04eba1b05a
armanghosh10 <149370200+armanghosh10@users.noreply.github.com>
2024-01-09 00:49:45 +0530

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -53,3 +53,27 @@ classifier.train(
 
 eval_result = classifier.evaluate(input_fn=lambda: input_fn(test, test_y, training=False))
 print('\nTest Set Accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+
+def input_fn(features, batch_size=256):
+    # Convert the inputs to a Dataset without labels.
+    return tf.data.Dataset.from_tensor_slices(dict(features)).batch(batch_size)
+
+features = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth']
+predict = {}
+
+print("Please type numeric values as prompted.")
+for feature in features:
+  valid = True
+  while valid: 
+    val = input(feature + ": ")
+    if not val.isdigit(): valid = False
+
+  predict[feature] = [float(val)]
+
+predictions = classifier.predict(input_fn=lambda: input_fn(predict))
+for pred_dict in predictions:
+    class_id = pred_dict['class_ids'][0]
+    probability = pred_dict['probabilities'][class_id]
+
+    print('Prediction is "{}" ({:.1f}%)'.format(
+        species[class_id], 100 * probability))
